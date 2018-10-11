56d75b94da03dd82b87c6ddffb245990178338c5
Ailton Silva <ailtonsscarvalho@gmail.com>
2018-10-11 11:22:29 -0300

    RPG Orientado a objetos
    Rpg de perguntas baseado em respostas de numeros, orientado a objetos.

---
+++ Rpg do prof/Rpg do prof/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rpg_do_prof
+{
+    class Program
+    {
+        public enum Escolha
+        {
+            Atacar = 0,
+            Fugir = 1,
+            Ver_inventário = 2,
+            Ver_habilidades = 3
+        }
+
+        public static bool lutando = true;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                string nome = Pergunta.RetornoTexto("Qual o seu nome?");
+                string raca = Pergunta.RetornoTexto("Qual a sua raça?");
+                Heroi h = new Heroi(30, nome, raca);
+
+                while (true)
+                {
+                    Console.Clear();
+
+                    string mensagem = "";
+                    Escolha[] elementos = Enum.GetValues(typeof(Escolha)) as Escolha[];
+                    foreach (Escolha n in elementos)
+                    {
+                        int valor = (int)n;
+                        string texto = n.ToString();
+                        mensagem += valor + " => " + texto + "\n";
+                        mensagem = mensagem.Replace('_', ' ');
+                    }
+
+                    int escolha = 0;
+                    do
+                    {
+                        s.getInfo();
+                        h.getInfo();  
+                        Console.WriteLine(mensagem);
+                        escolha = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        switch (escolha)
+                        {
+                            case 0:
+                                Console.WriteLine("Você escolheu atacar.");
+                                Console.WriteLine(h.atacar(s));
+                                break;
+                            case 1:
+                                Console.WriteLine("Você escolheu fugir.");
+                                break;
+                        }
+
+                        Console.WriteLine();
+                    } while (lutando);
+
+                    Console.ReadLine();
+                }
+                Random r = new Random();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Rpg do prof/Rpg do prof/TodosOsObjetos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rpg_do_prof
+{
+    class TodosOsObjetos
+    {
+        public static List<Item> item = new List<Item>();
+        public static List<Item> armasIniciais = new List<Item>();
+        public static List<SerVivo> inimigo = new List<SerVivo>();
+
+        static TodosOsObjetos()
+        {
+
+
+            #region Inimigos
+            //Inimigos
+            inimigo.Add(new SerVivo(10, "Slime azul"));
+            inimigo.Add(new SerVivo(10, "Slime roxo"));
+            #endregion Inimigos
+
+            #region Todos os Itens
+            //Itens
+            item.Add(new Item(1, 3, "Ordinaria Espada"));
+            #endregion Todos os Itens
+
+            #region Itens Iniciais
+            //Itens do começo do jogo.
+            armasIniciais.Add(new Item(1, 3, "Espada Ordinaria de Madeira"));
+            armasIniciais.Add(new Item(2, 4, "Espada de Cobre enferrujada"));
+            armasIniciais.Add(new Item(5,0,0,"Elixir de vida superior", "Um elixir de vida que pode curar qualquer doenças"));
+
+            #endregion Itens Iniciais
+        }
+    }
+}
