7ee49b284e3a6f54fd6a1dfc15093a5831815f2e
Someone <ailtonsscarvalho@gmail.com>
2018-10-11 21:02:58 -0300

    Itens novos adicionado por Topeira

--- RPG/RPG Definitivo/RPG Definitivo/TodosOsObjetos.cs
+++ RPG/RPG Definitivo/RPG Definitivo/TodosOsObjetos.cs
@@ -19,6 +19,7 @@ namespace RPG_Definitivo
             //Inimigos
             inimigo.Add(new Inimigo(100,20, "Slime azul", 2));
             inimigo.Add(new Inimigo(50,10, "Slime roxo"));
+            inimigo.Add(new Inimigo(200, 15, "Goblin"));
             #endregion Inimigos
 
             #region Todos os Itens
@@ -29,8 +30,8 @@ namespace RPG_Definitivo
             #region Itens Iniciais
             //Itens do começo do jogo.
             armasIniciais.Add(new Item(1, 3, "Espada Ordinaria de Madeira"));
-            armasIniciais.Add(new Item(2, 4, "Espada de Cobre enferrujada"));
-
+            armasIniciais.Add(new Item(1, 4, "Espada de Cobre enferrujada"));
+            armasIniciais.Add(new Item(1, 3, "Arco Curto de Madeira Podre"));
             #endregion Itens Iniciais
         }
     }
